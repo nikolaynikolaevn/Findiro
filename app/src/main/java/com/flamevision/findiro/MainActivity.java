4916a9de9bc8c6dcf2a7f30747937454133c701e
Theo <t.xu@student.fontys.nl>
2020-05-13 15:20:12 +0200

    new login and sigh up layout

--- app/src/main/java/com/flamevision/findiro/MainActivity.java
+++ app/src/main/java/com/flamevision/findiro/MainActivity.java
@@ -11,8 +11,11 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.flamevision.findiro.LoginAndSignup.TestLoginAndSignupActivity;
@@ -39,7 +42,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
 
         btnTestUserAndGroup = findViewById(R.id.mainTestUserAndGroupButton);
         btnTestUserAndGroup.setOnClickListener(new View.OnClickListener() {
