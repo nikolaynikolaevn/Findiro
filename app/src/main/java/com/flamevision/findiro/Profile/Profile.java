e9c5584c896bbdfc3e550a5745cc1d8629270cc2
Theo <t.xu@student.fontys.nl>
2020-05-15 00:11:08 +0200

    updated profile UI and profile.class but not done yet

--- app/src/main/java/com/flamevision/findiro/Profile/Profile.java
+++ app/src/main/java/com/flamevision/findiro/Profile/Profile.java
@@ -1,4 +1,112 @@
 package com.flamevision.findiro.Profile;
 
-public class Profile {
+import android.app.AppComponentFactory;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.flamevision.findiro.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Profile extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener
+
+{
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+    private static final int MAP_PERMISSION = 001;
+    private GoogleApiClient googleApiClient;
+    private FirebaseAuth auth;
+    private FirebaseUser currentUser;
+    String emailAddress;
+    TextView userName;
+    ImageView profileImage;
+    static String photoUrl;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_activity);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+
+        //another stuff
+        profileImage = (ImageView)findViewById(R.id.ivProfileImage);
+        userName=findViewById(R.id.tvMyUser);
+
+        auth = FirebaseAuth.getInstance();
+        currentUser = auth.getCurrentUser();
+    }
+
+    public void startEditProfile(View view) {
+
+    }
+
+    public void onBackEvent(View view) {
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+        System.out.println("entering onStart");
+        //get the picture method
+        getPhotoUrl();
+    }
+
+
+    private void getPhotoUrl() {
+
+    }
+
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, this.getMainLooper());
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, MAP_PERMISSION);
+        }
+    }
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
